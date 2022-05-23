0ba1463f80c7f529da532687e63c6c87f9463ab0
mdrokib231185 <mdrokhasan231185@gmail.com>
2022-05-23 13:15:56 +0600

    single data load

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.812m2.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const productsCollection = client.db("assignment-12").collection('products');
+
+    app.get("/products", async (req, res) => {
+      const product = await productsCollection.find().toArray();
+      res.send(product);
+    });
+        
+        
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const products = await productsCollection.findOne(query);
+      res.send(products);
+    });
+        
+        
+        
+        
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Assignment listening on port ${port}`);
+});
