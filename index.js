a12f289da0ad70235ac60cde1e8f30eb8efc3170
mdrokib231185 <mdrokhasan231185@gmail.com>
2022-05-25 15:58:20 +0600

    user add

--- index.js
+++ index.js
@@ -19,11 +19,34 @@ async function run() {
     await client.connect();
     const productsCollection = client.db("assignment-12").collection('products');
     const bookingCollection = client.db("assignment-12").collection('booking');
+    const userCollection = client.db("assignment-12").collection('users');
 
     app.get("/products", async (req, res) => {
       const product = await productsCollection.find().toArray();
       res.send(product);
     });
+
+    app.put("/user/:email", async (req, res) => {
+      const user = req.body;
+      const email = req.params.email;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
+
+
+    app.get('/booking', async(req, res) => {
+      const customer = req.query.customer
+      const query = { customer: customer }
+      const bookings = await bookingCollection.find(query).toArray()
+      res.send(bookings)
+    })
+
+
     app.post('/booking', async (req, res) => {
       const booking = req.body
       const result = await bookingCollection.insertOne(booking)
