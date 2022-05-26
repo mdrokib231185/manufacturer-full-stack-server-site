287011c5242e4cf0d4a3d5cccceef67a7aff33de
mdrokib231185 <mdrokhasan231185@gmail.com>
2022-05-26 15:25:45 +0600

    deleted add

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId, ObjectID } = require("mongodb");
 const jwt = require("jsonwebtoken");
+const query = require("express/lib/middleware/query");
 
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.812m2.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -45,6 +46,12 @@ async function run() {
     app.get("/products", async (req, res) => {
       const product = await productsCollection.find().toArray();
       res.send(product);
+    });
+
+    app.post("/products", async (req, res) => {
+      const newProduct = req.body;
+      const result = await productsCollection.insertOne(newProduct);
+      res.send(result);
     });
 
     app.get("/user", verifyJWT, async (req, res) => {
@@ -92,6 +99,7 @@ async function run() {
       res.send({admin : isAdmin})
 })
 
+    
 
     app.get("/booking", verifyJWT, async (req, res) => {
       const customer = req.query.customer;
@@ -109,6 +117,14 @@ async function run() {
     app.post("/booking", async (req, res) => {
       const booking = req.body;
       const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    app.delete("/booking/:id", verifyJWT, async (req, res) => {
+     const id= req.params._id
+     const query = { _id: ObjectId(id) }
+      const filter={query: query}
+      const result = await bookingCollection.deleteOne(filter);
       res.send(result);
     });
 
